56c3723a3d229456ee5093a4be1280d82702db44
Benjamin Czaja <benjamic@tcn1.local.snellius.surf.nl>
2024-02-06 14:09:51 +0100

    adding benchmark python script

--- scripts/benchmarker.py
+++ scripts/benchmarker.py
@@ -1,16 +1,44 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import os
 import subprocess
 import re 
+import pandas as pd
 
 #Goal 1: benchmark time vs size
 
 # QUESTIOn Benchmark in Python or make script to benchmark?????
 
 
+def log_results(result):
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    results_dir = script_dir.replace("scripts","results")
+    results_file = results_dir + "/results.csv"
+
+    data = pd.DataFrame(result)
+
+
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+
+    if os.path.isfile(results_file):
+        data_tmp = pd.read_csv(results_file)
+        print("read in")
+        print(data_tmp)
+        data = pd.concat([data_tmp, data],ignore_index=True)
+        data = data.sort_values(by='Name')
+        print("data new")
+        print(data)
+        data.to_csv(results_file,sep=',',index=False)
+    else:
+        data.to_csv(results_file,sep=',',index=False)
+
+    print(data)
+
 
 def benchmark(executable, size, args=None, env_var=None):
+
     # get the path of this python script
     script_dir = os.path.dirname(os.path.realpath(__file__))
     bin_dir = script_dir.replace("scripts","bin")
@@ -31,8 +59,6 @@ def benchmark(executable, size, args=None, env_var=None):
         output = subprocess.check_output([executable, str(size), str(args)]).decode("utf-8")
     else:
         output = subprocess.check_output([executable, str(size), str(args)]).decode("utf-8")
-
-
     
     # regex the results:
     number = r'[+-]?((\d+\.\d*)|(\.\d+)|(\d+))([eE][+-]?\d+)?'
@@ -45,13 +71,29 @@ def benchmark(executable, size, args=None, env_var=None):
     x = re.search(time_pattern, output,re.MULTILINE)
     result_time = x['rtime']
 
-    print(result_size, result_time)
+    nan = float('nan')
+    # save the results
+    result = {
+    "Name": [executable.split("/")[-1]],
+    "Args": [args],
+    "env_var": [env_var],
+    "Size": [result_size],
+    "Total_time": [result_time],
+    "CPU_time": [nan],
+    "GPU_time": [nan],
+    "Total_energy": [nan],
+    "CPU_energy": [nan],
+    "GPU_energy": [nan],
+
+    }
+
+    log_results(result)
 
 
 if __name__ == "__main__":
 
-    matrix_sizes = range(0,1000,100)
+    matrix_sizes = range(0,1800,100)
 
     for size in matrix_sizes:
-        benchmark("dgemm",size, "-s")
+        benchmark("dgemm",size, "-p", env_var="OMP_NUM_THREADS=8")
 
