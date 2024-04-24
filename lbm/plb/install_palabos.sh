d4d852360c721eddf3679d3e216e8f193829975f
Benjamin Czaja <benjamic@tcn1.local.snellius.surf.nl>
2024-04-24 13:20:31 +0200

    update CMakeLists

--- lbm/plb/install_palabos.sh
+++ lbm/plb/install_palabos.sh
@@ -4,10 +4,13 @@ module load 2023
 module load foss/2023a
 module load CMake/3.26.3-GCCcore-12.3.0
 
+cd ..
+
 git clone https://gitlab.com/unigespc/palabos.git
 
-cp aneurysm.cpp  palabos/examples/showCases/aneurysm/
-cp input_1_node*  palabos/examples/showCases/aneurysm/
+cp plb/aneurysm.cpp  palabos/examples/showCases/aneurysm/
+cp plb/input_1_node*  palabos/examples/showCases/aneurysm/
+cp plb/CMakeLists.txt  palabos/examples/showCases/aneurysm/
 
 cd palabos/examples/showCases/aneurysm/build
 
