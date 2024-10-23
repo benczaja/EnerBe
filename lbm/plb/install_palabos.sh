da3db61e08b286e064a95b4568cbc6e65dca68d4
benjamic <benjamin.czaja@surf.nl>
2024-10-23 09:33:17 +0200

    updated PLB build

--- lbm/plb/install_palabos.sh
+++ lbm/plb/install_palabos.sh
@@ -4,9 +4,14 @@
 module load 2023
 module load foss/2023a
 module load CMake/3.26.3-GCCcore-12.3.0
-module load pmt/1.2.0-GCCcore-12.3.0
+#module load pmt/1.2.0-GCCcore-12.3.0
+
+#local build
+export LIBRARY_PATH=$LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
+
+
 #LIZA stuff
-#export LIBRARY_PATH=$LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
 #cmake -DENABLE_PMT=1 -DPMT_ROOT=/home/benjamic/EnerBe/pmt -DCMAKE_PREFIX_PATH=/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/generic/software/OpenMPI/4.1.4-GCC-12.2.0 ..
 #Needed at runtime (maybe could consider setting RPATH)
 #export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
@@ -21,7 +26,8 @@ cp plb/input_1_node*  ../bin
 
 cd palabos/examples/showCases/aneurysm/build
 
-cmake -DENABLE_PMT=1 ..
+#cmake -DENABLE_PMT=1 ..
+cmake -DENABLE_PMT=1 -DPMT_ROOT=/home/benjamic/EnerBe/pmt -DCMAKE_PREFIX_PATH=/home/benjamic/EnerBe/pmt ..
 # LIZA
 #cmake -DENABLE_PMT=1 -DPMT_ROOT=/home/benjamic/EnerBe/pmt -DCMAKE_PREFIX_PATH=/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/generic/software/OpenMPI/4.1.4-GCC-12.2.0 ..
 
