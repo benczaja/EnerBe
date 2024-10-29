069f60f507b0f51fda7bb9c5f7a52ccaf1bb5f47
benjamic <benjamin.czaja@surf.nl>
2024-10-29 14:08:12 +0100

    update PLB build

--- lbm/plb/install_palabos.sh
+++ lbm/plb/install_palabos.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
 #Snellius stuff
-module load 2023
-module load foss/2023a
-module load CMake/3.26.3-GCCcore-12.3.0
+#module load 2023
+module load foss/2023b
+#module load CMake/3.26.3-GCCcore-12.3.0
 #module load pmt/1.2.0-GCCcore-12.3.0
 
-#local build
+# in case you are not working with modules
 export LIBRARY_PATH=$LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
+export LIBRARY_PATH=$LIBRARY_PATH:/opt/rocm/lib
+export LIBRARY_PATH=$LIBRARY_PATH:/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/amd/zen3/software/GCCcore/13.2.0/lib64
+export LIBRARY_PATH=$LIBRARY_PATH:/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/amd/zen3/software/OpenMPI/4.1.6-GCC-13.2.0/lib
+
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/amd/zen3/software/GCCcore/13.2.0/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/amd/zen3/software/OpenMPI/4.1.6-GCC-13.2.0/lib
 
-
-#LIZA stuff
-#cmake -DENABLE_PMT=1 -DPMT_ROOT=/home/benjamic/EnerBe/pmt -DCMAKE_PREFIX_PATH=/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/generic/software/OpenMPI/4.1.4-GCC-12.2.0 ..
-#Needed at runtime (maybe could consider setting RPATH)
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/benjamic/EnerBe/pmt/lib
 
 cd ..
 
@@ -26,10 +29,7 @@ cp plb/input_1_node*  ../bin
 
 cd palabos/examples/showCases/aneurysm/build
 
-#cmake -DENABLE_PMT=1 ..
 cmake -DENABLE_PMT=1 -DPMT_ROOT=/home/benjamic/EnerBe/pmt -DCMAKE_PREFIX_PATH=/home/benjamic/EnerBe/pmt ..
-# LIZA
-#cmake -DENABLE_PMT=1 -DPMT_ROOT=/home/benjamic/EnerBe/pmt -DCMAKE_PREFIX_PATH=/cvmfs/software.eessi.io/versions/2023.06/software/linux/x86_64/generic/software/OpenMPI/4.1.4-GCC-12.2.0 ..
 
 make -j 
 
@@ -37,7 +37,7 @@ cd ../
 
 tar -xzvf aneurysm.stl.tgz
 
-cp aneurysm ../../../../../bin/aneurysm_pmt
+cp aneurysm ../../../../../bin/aneurysm
 cp aneurysm.stl ../../../../../bin/
 
 cd ../../../../../
