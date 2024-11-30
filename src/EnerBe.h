df870fac7cdf7076c53eb062298fe5eca4478bb8
benjamic <benjamin.czaja@surf.nl>
2024-11-30 20:54:12 +0100

    some cleaning up

--- src/EnerBe.h
+++ src/EnerBe.h
@@ -22,6 +22,7 @@ class EnerBe {
     std::string name;
     std::string algorithm;
     std::string perf_unit = "UNK";
+    std::string gpu_vendor = "UNK";
     
     int omp_threads = omp_get_max_threads();
     int gpuid = 99;
@@ -41,43 +42,33 @@ class EnerBe {
     double time_std = 0.0;
     double perf = 0;
 
-
     // Devil horns
     double sign_of_the_beast = 0.0;
 
     double rapl_time = 0.0;
-    double nvml_time = 0.0;
-    double rocm_time = 0.0;
+    double gpu_time = 0.0;
 
     double rapl_power = 0.0;
-    double nvml_power = 0.0;
-    double rocm_power = 0.0;
+    double gpu_power = 0.0;
 
     double rapl_energy = 0.0;
-    double nvml_energy = 0.0;
-    double rocm_energy = 0.0;
+    double gpu_energy = 0.0;
 
     double rapl_time_var = 0.0;
-    double nvml_time_var = 0.0;
-    double rocm_time_var = 0.0;
+    double gpu_time_var = 0.0;
     double rapl_time_std = 0.0;
-    double nvml_time_std = 0.0;
-    double rocm_time_std = 0.0;
+    double gpu_time_std = 0.0;
 
     double rapl_power_var = 0.0;
-    double nvml_power_var = 0.0;
-    double rocm_power_var = 0.0;
+    double gpu_power_var = 0.0;
     double rapl_power_std = 0.0;
-    double nvml_power_std = 0.0;
-    double rocm_power_std = 0.0;
+    double gpu_power_std = 0.0;
 
     double rapl_energy_var = 0.0;
-    double nvml_energy_var = 0.0;
-    double rocm_energy_var = 0.0;
+    double gpu_energy_var = 0.0;
 
     double rapl_energy_std = 0.0;
-    double nvml_energy_std = 0.0;
-    double rocm_energy_std = 0.0;
+    double gpu_energy_std = 0.0;
 
     #if defined(PMT_ENABLED) && defined(CUDA_ENABLED)
         //std::unique_ptr<pmt::PMT> GPUsensor = pmt::Create("NVML");
@@ -113,21 +104,18 @@ class EnerBe {
     double rapl_powers[20] = {sign_of_the_beast};
     double rapl_energys[20] = {sign_of_the_beast};
 
-    double nvml_times[20] =  {sign_of_the_beast};
-    double nvml_powers[20] = {sign_of_the_beast};
-    double nvml_energys[20] = {sign_of_the_beast};
-
-    double rocm_times[20] =  {sign_of_the_beast};
-    double rocm_powers[20] = {sign_of_the_beast};
-    double rocm_energys[20] = {sign_of_the_beast};
-
+    double gpu_times[20] =  {sign_of_the_beast};
+    double gpu_powers[20] = {sign_of_the_beast};
+    double gpu_energys[20] = {sign_of_the_beast};
 
     void print_info(){
 
         #if defined(PMT_ENABLED) && defined(CUDA_ENABLED)
-            print_pmt_nvml_info();
+            gpu_vendor = "NVML";
+            print_pmt_gpu_info();
         #elif defined(PMT_ENABLED) && defined(HIP_ENABLED)
-            print_pmt_rocm_info();
+            gpu_vendor = "ROCM";
+            print_pmt_gpu_info();
         #elif defined(PMT_ENABLED)
             print_pmt_rapl_info();
         #else
@@ -163,41 +151,25 @@ class EnerBe {
         std::cout << "(RAPL) CPU_JOULES: " << rapl_energy << " J" << std::endl;
         std::cout << "(RAPL) CPU_JOULES_var: " << rapl_energy_var << " J^2" << std::endl;
         std::cout << "(RAPL) CPU_JOULES_std: " << rapl_energy_std << " J" << std::endl;
-        std::cout << "TOTAL_WATTS: " << rapl_power + nvml_power << " W"<< std::endl;
-        std::cout << "TOTAL_JOULES: " << rapl_energy + nvml_energy << " J"<< std::endl;
+        std::cout << "TOTAL_WATTS: " << rapl_power + gpu_power << " W"<< std::endl;
+        std::cout << "TOTAL_JOULES: " << rapl_energy + gpu_energy << " J"<< std::endl;
 
     } 
 
-    void print_pmt_nvml_info() { 
+    void print_pmt_gpu_info() { 
         print_basic_info();
-        std::cout << "(RAPL) CPU_TIME: " << rapl_time << " | (NVML) GPU_TIME: " << nvml_time << " s"<< std::endl;
-        std::cout << "(RAPL) CPU_TIME_var: " << rapl_time_var << " | (NVML) GPU_TIME_var: " << nvml_time_var << " s^2"<< std::endl;
-        std::cout << "(RAPL) CPU_TIME_std: " << rapl_time_std << " | (NVML) GPU_TIME_std: " << nvml_time_std << " s"<< std::endl;
-        std::cout << "(RAPL) CPU_WATTS: " << rapl_power << " | (NVML) GPU_WATTS: " << nvml_power << " W"<< std::endl;
-        std::cout << "(RAPL) CPU_WATTS_var: " << rapl_power_var << " | (NVML) GPU_WATTS_var: " << nvml_power_var << " W^2"<< std::endl;
-        std::cout << "(RAPL) CPU_WATTS_std: " << rapl_power_std << " | (NVML) GPU_WATTS_std: " << nvml_power_std << " W"<< std::endl;
-        std::cout << "(RAPL) CPU_JOULES: " << rapl_energy << " | (NVML) GPU_JOULES: " << nvml_energy << " J"<< std::endl;
-        std::cout << "(RAPL) CPU_JOULES_var: " << rapl_energy_var << " | (NVML) GPU_JOULES_var: " << nvml_energy_var << " J^2"<< std::endl;
-        std::cout << "(RAPL) CPU_JOULES_std: " << rapl_energy_std << " | (NVML) GPU_JOULES_std: " << nvml_energy_std << " J"<< std::endl;
-        std::cout << "TOTAL_WATTS: " << rapl_power + nvml_power << " W"<< std::endl;
-        std::cout << "TOTAL_JOULES: " << rapl_energy + nvml_energy << " J"<< std::endl;
+        std::cout << "(RAPL) CPU_TIME: " << rapl_time << " | (" << gpu_vendor << ") GPU_TIME: " << gpu_time << " s"<< std::endl;
+        std::cout << "(RAPL) CPU_TIME_var: " << rapl_time_var << " | (" << gpu_vendor << ") GPU_TIME_var: " << gpu_time_var << " s^2"<< std::endl;
+        std::cout << "(RAPL) CPU_TIME_std: " << rapl_time_std << " | (" << gpu_vendor << ") GPU_TIME_std: " << gpu_time_std << " s"<< std::endl;
+        std::cout << "(RAPL) CPU_WATTS: " << rapl_power << " | (" << gpu_vendor << ") GPU_WATTS: " << gpu_power << " W"<< std::endl;
+        std::cout << "(RAPL) CPU_WATTS_var: " << rapl_power_var << " | (" << gpu_vendor << ") GPU_WATTS_var: " << gpu_power_var << " W^2"<< std::endl;
+        std::cout << "(RAPL) CPU_WATTS_std: " << rapl_power_std << " | (" << gpu_vendor << ") GPU_WATTS_std: " << gpu_power_std << " W"<< std::endl;
+        std::cout << "(RAPL) CPU_JOULES: " << rapl_energy << " | (" << gpu_vendor << ") GPU_JOULES: " << gpu_energy << " J"<< std::endl;
+        std::cout << "(RAPL) CPU_JOULES_var: " << rapl_energy_var << " | (" << gpu_vendor << ") GPU_JOULES_var: " << gpu_energy_var << " J^2"<< std::endl;
+        std::cout << "(RAPL) CPU_JOULES_std: " << rapl_energy_std << " | (" << gpu_vendor << ") GPU_JOULES_std: " << gpu_energy_std << " J"<< std::endl;
+        std::cout << "TOTAL_WATTS: " << rapl_power + gpu_power << " W"<< std::endl;
+        std::cout << "TOTAL_JOULES: " << rapl_energy + gpu_energy << " J"<< std::endl;
     } 
-
-    void print_pmt_rocm_info() { 
-        print_basic_info();
-        std::cout << "(RAPL) CPU_TIME: " << rapl_time << " | (ROCM) GPU_TIME: " << rocm_time << " s"<< std::endl;
-        std::cout << "(RAPL) CPU_TIME_var: " << rapl_time_var << " | (ROCM) GPU_TIME_var: " << rocm_time_var << " s^2"<< std::endl;
-        std::cout << "(RAPL) CPU_TIME_std: " << rapl_time_std << " | (ROCM) GPU_TIME_std: " << rocm_time_std << " s"<< std::endl;
-        std::cout << "(RAPL) CPU_WATTS: " << rapl_power << " | (ROCM) GPU_WATTS: " << rocm_power << " W"<< std::endl;
-        std::cout << "(RAPL) CPU_WATTS_var: " << rapl_power_var << " | (ROCM) GPU_WATTS_var: " << rocm_power_var << " W^2"<< std::endl;
-        std::cout << "(RAPL) CPU_WATTS_std: " << rapl_power_std << " | (ROCM) GPU_WATTS_std: " << rocm_power_std << " W"<< std::endl;
-        std::cout << "(RAPL) CPU_JOULES: " << rapl_energy << " | (ROCM) GPU_JOULES: " << rocm_energy << " J"<< std::endl;
-        std::cout << "(RAPL) CPU_JOULES_var: " << rapl_energy_var << " | (ROCM) GPU_JOULES_var: " << rocm_energy_var << " J^2"<< std::endl;
-        std::cout << "(RAPL) CPU_JOULES_std: " << rapl_energy_std << " | (ROCM) GPU_JOULES_std: " << rocm_energy_std << " J"<< std::endl;
-        std::cout << "Total WATTS: " << rapl_power + rocm_power << " W"<< std::endl;
-        std::cout << "Total JOULES: " << rapl_energy + rocm_energy << " J"<< std::endl;
-    } 
-
 
     void measure(){
 
@@ -227,9 +199,9 @@ class EnerBe {
                 rapl_powers[N_runs] = pmt::PMT::watts(CPUstart, CPUend);
                 rapl_energys[N_runs] = pmt::PMT::joules(CPUstart, CPUend);
 
-                nvml_times[N_runs] = pmt::PMT::seconds(GPUstart, GPUend);
-                nvml_powers[N_runs] = pmt::PMT::watts(GPUstart, GPUend);
-                nvml_energys[N_runs] = pmt::PMT::joules(GPUstart, GPUend);
+                gpu_times[N_runs] = pmt::PMT::seconds(GPUstart, GPUend);
+                gpu_powers[N_runs] = pmt::PMT::watts(GPUstart, GPUend);
+                gpu_energys[N_runs] = pmt::PMT::joules(GPUstart, GPUend);
 
             #elif defined(PMT_ENABLED) && defined(HIP_ENABLED)
                 GPUend = GPUsensor->Read();
@@ -239,9 +211,9 @@ class EnerBe {
                 rapl_powers[N_runs] = pmt::PMT::watts(CPUstart, CPUend);
                 rapl_energys[N_runs] = pmt::PMT::joules(CPUstart, CPUend);
 
-                rocm_times[N_runs] = pmt::PMT::seconds(GPUstart, GPUend);
-                rocm_powers[N_runs] = pmt::PMT::watts(GPUstart, GPUend);
-                rocm_energys[N_runs] = pmt::PMT::joules(GPUstart, GPUend);
+                gpu_times[N_runs] = pmt::PMT::seconds(GPUstart, GPUend);
+                gpu_powers[N_runs] = pmt::PMT::watts(GPUstart, GPUend);
+                gpu_energys[N_runs] = pmt::PMT::joules(GPUstart, GPUend);
 
             #elif defined(PMT_ENABLED)
                 CPUend = CPUsensor->Read();
@@ -263,18 +235,15 @@ class EnerBe {
         
         double sum_time = 0.0;
         double sum_rapl_time = 0.0;
-        double sum_nvml_time = 0.0;
-        double sum_rocm_time = 0.0;
+        double sum_gpu_time = 0.0;
 
         double sum_energy = 0.0;
         double sum_rapl_energy = 0.0;
-        double sum_nvml_energy = 0.0;
-        double sum_rocm_energy = 0.0;
+        double sum_gpu_energy = 0.0;
 
         double sum_power = 0.0;
         double sum_rapl_power = 0.0;
-        double sum_nvml_power = 0.0;
-        double sum_rocm_power = 0.0;
+        double sum_gpu_power = 0.0;
 
         for(int i=0; i<N_runs ;i++){
             sum_time += times[i];
@@ -283,87 +252,71 @@ class EnerBe {
             sum_rapl_power  += rapl_powers[i];
             sum_rapl_energy += rapl_energys[i];
 
-            sum_nvml_time   += nvml_times[i];
-            sum_nvml_power  += nvml_powers[i];
-            sum_nvml_energy += nvml_energys[i];
-
-            sum_rocm_time   += rocm_times[i];
-            sum_rocm_power  += rocm_powers[i];
-            sum_rocm_energy += rocm_energys[i];
+            sum_gpu_time   += gpu_times[i];
+            sum_gpu_power  += gpu_powers[i];
+            sum_gpu_energy += gpu_energys[i];
 
         }
 
         rapl_time = sum_rapl_time/double(N_runs);
-        nvml_time = sum_nvml_time/double(N_runs);
-        rocm_time = sum_rocm_time/double(N_runs);
+        gpu_time = sum_gpu_time/double(N_runs);
 
-        time = rapl_time + nvml_time + rocm_time;
+        time = (rapl_time + gpu_time); // Maybe this is wrong??
 
         rapl_power = sum_rapl_power/double(N_runs);
-        nvml_power = sum_nvml_power/double(N_runs);
-        rocm_power = sum_rocm_power/double(N_runs);
+        gpu_power = sum_gpu_power/double(N_runs);
 
         rapl_energy = sum_rapl_energy/double(N_runs);
-        nvml_energy = sum_nvml_energy/double(N_runs);
-        rocm_energy = sum_rocm_energy/double(N_runs);
+        gpu_energy = sum_gpu_energy/double(N_runs);
 
         double values = 0;
         double rapl_time_values = 0;
-        double nvml_time_values = 0;
-        double rocm_time_values = 0;
+        double gpu_time_values = 0;
 
         double rapl_power_values = 0;
-        double nvml_power_values = 0;
-        double rocm_power_values = 0;
+        double gpu_power_values = 0;
 
         double rapl_energy_values = 0;
-        double nvml_energy_values = 0;
-        double rocm_energy_values = 0;
+        double gpu_energy_values = 0;
+
         for(int i = 0; i < N_runs; i++) {
             values += pow(times[i] - time, 2);
 
             rapl_time_values += pow(rapl_times[i] - rapl_time, 2);
-            nvml_time_values += pow(nvml_times[i] - nvml_time, 2);
-            rocm_time_values += pow(rocm_times[i] - rocm_time, 2);
+            gpu_time_values += pow(gpu_times[i] - gpu_time, 2);
 
             rapl_power_values += pow(rapl_powers[i] - rapl_power, 2);
-            nvml_power_values += pow(nvml_powers[i] - nvml_power, 2);
-            rocm_power_values += pow(rocm_powers[i] - rocm_power, 2);
+            gpu_power_values += pow(gpu_powers[i] - gpu_power, 2);
 
             rapl_energy_values += pow(rapl_energys[i] - rapl_energy, 2);
-            nvml_energy_values += pow(nvml_energys[i] - nvml_energy, 2);
-            rocm_energy_values += pow(rocm_energys[i] - rocm_energy, 2);
+            gpu_energy_values += pow(gpu_energys[i] - gpu_energy, 2);
         }
     
         // variance is the square of standard deviation
 
         rapl_time_var = rapl_time_values /double(N_runs);
-        nvml_time_var = nvml_time_values /double(N_runs);
-        rocm_time_var = rocm_time_values /double(N_runs);
+        gpu_time_var = gpu_time_values /double(N_runs);
 
         rapl_power_var = rapl_power_values /double(N_runs);
-        nvml_power_var = nvml_power_values /double(N_runs);
-        rocm_power_var = rocm_power_values /double(N_runs);
+        gpu_power_var = gpu_power_values /double(N_runs);
 
         rapl_energy_var = rapl_energy_values /double(N_runs);
-        nvml_energy_var = nvml_energy_values /double(N_runs);
-        rocm_energy_var = rocm_energy_values /double(N_runs);
+        gpu_energy_var = gpu_energy_values /double(N_runs);
 
-        time_var = rapl_time_var + nvml_time_var + rocm_time_var;
+        time_var = rapl_time_var + gpu_time_var;
 
         // calculating standard deviation by finding square root
         // of variance
         time_std = sqrt(time_var);
 
         rapl_time_std   = sqrt(rapl_time_var);
-        nvml_time_std   = sqrt(nvml_time_var);
-        rocm_time_std   = sqrt(rocm_time_var);
+        gpu_time_std   = sqrt(gpu_time_var);
+
         rapl_power_std  = sqrt(rapl_power_var);
-        nvml_power_std  = sqrt(nvml_power_var);
-        rocm_power_std  = sqrt(rocm_power_var);
+        gpu_power_std  = sqrt(gpu_power_var);
+
         rapl_energy_std = sqrt(rapl_energy_var);
-        nvml_energy_std = sqrt(nvml_energy_var);
-        rocm_energy_std = sqrt(rocm_energy_var);
+        gpu_energy_std = sqrt(gpu_energy_var);
 
         
         if (name == "xgemm")
